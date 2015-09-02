2d2b868250afd3adb49a41f5ca22269c5ebff84a
droobey <droobey@users.noreply.github.com>
2015-09-02 14:10:27 +0100

    Update sips_chat_filter.meta.js

--- sips_chat_filter.meta.js
+++ sips_chat_filter.meta.js
@@ -3,9 +3,9 @@
 // @namespace   https://github.com/droobey/sips-chat-filter
 // @description Tracks and hides !bets from Sips twitch chat. Made for /r/sips
 // @author      /u/droobey
-// @updateURL   http://droobey.github.io/sips-chat-filter/sips_chat_filter.meta.js
+// @updateURL   https://github.com/droobey/sips-chat-filter/blob/master/sips_chat_filter.user.js
 // @include     /^https?://(www|beta)\.twitch\.tv\/(sips_(/(chat.*)?)?|chat\/.*channel=sips_.*)$/
-// @version     1.0
+// @version     1.01
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
